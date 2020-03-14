8ac3a0b083cf03ecad115942b313800e37b8a274
Rodolfo Araújo <rodolfo@pop-os.localdomain>
2020-03-14 17:42:32 -0300

    Adding the description for parity outline challenge

--- lib/parity_outliner.ex
+++ lib/parity_outliner.ex
@@ -1,4 +1,12 @@
 defmodule ParityOutliner do
+    @moduledoc """
+    You are given an array (which will have a length of at least 3, 
+    but could be very large) containing integers. The array is either 
+    entirely comprised of odd integers or entirely comprised of even 
+    integers except for a single integer N. Write a method that takes 
+    the array as an argument and returns this "outlier" N.
+    """
+    
     def find_outlier(integers) do
         countOdds = Enum.filter(integers, fn n -> rem(n,2) == 1 end)
         countEven = Enum.filter(integers, fn n -> rem(n,2) == 0 end)
